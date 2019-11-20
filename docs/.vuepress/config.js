68edec88e5a57ad9d6505c6c0b5dcb4ad9be7201
yuanjz <yuanjz@deepblueai.com>
2019-11-20 16:27:57 +0800

    加了一个养猪场的md 以及about md

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -2,11 +2,10 @@ module.exports = {
   title: 'AZHONG',
   description: '学海无涯,真香!',
   themeConfig: {
-    // // 1.接受字符串，它设置了最后更新时间的label，例如：最后更新时间：2019年5月3日 21:51:53
-    // lastUpdated: '最后更新时间',
-    // // 2.设置true，开启最后更新时间
-    // lastUpdated: true,
-    lastUpdated: '上次更新',
+    // 1.接受字符串，它设置了最后更新时间的label，例如：最后更新时间：2019年5月3日 21:51:53
+    lastUpdated: '最后更新时间',
+    // 2.设置true，开启最后更新时间
+    lastUpdated: true,
     // GitHub仓库地址
     repo: 'https://github.com/yuanjinzhong',
     // 自定义仓库链接文字。
@@ -14,7 +13,6 @@ module.exports = {
     //导航栏
     nav: [
       { text: 'Home', link: '/' },
-      { text: 'FirstBlog', link: '/blog/FirstBlog.md' },
       {
         text: 'Languages',
         items: [
@@ -24,29 +22,15 @@ module.exports = {
           { text: 'Go', link: '/language/japanese' }
         ]
       },
-      {
-        text: 'Languages3',
-        items: [
-          {
-            text: '分组1', items: [{ text: 'Java', link: '/language/chinese' },
-            { text: 'H5', link: '/language/japanese' },
-            { text: 'Python', link: '/language/japanese' },
-            { text: 'Go', link: '/language/japanese' }]
-          },
-          {
-            text: '分组2', items: [{ text: 'Java', link: '/language/chinese' },
-            { text: 'H5', link: '/language/japanese' },
-            { text: 'Python', link: '/language/japanese' },
-            { text: 'Go', link: '/language/japanese' }]
-          }
-        ]
-      }, { text: '关于', link: '/' }
+      { text: 'About', link: '/blog/about.md' }
     ],
     //侧边栏
     sidebar: [
       ['/', '首页'],
       ['/blog/java基础/static关键字.md', 'static关键字'],
-      ['/blog/mybatis/mybatis.md', 'mybatis学习']
+      ['/blog/mybatis/mybatis.md', 'mybatis学习'],
+      ['/blog/FirstBlog.md', '特性测试'],
+      ['/blog/养猪场.md', '平安养殖场']
     ]
   }
 }
---
+++ docs/blog/about.md
@@ -0,0 +1,15 @@
+::: tip  
+坐标魔都
+:::
+# vuepress使用感受
+* 我感觉在vuepress在没有牛逼主题的加持下,只能作为接口文档分享,不适合做博客站点。
+* 最好有个主题开箱即用,有标签云,有评论,有时间线,有阅读和字数统计,赞赏，友链功能
+* vuepress的首页很好看,很简洁,真香。
+* 目前写做博客站点还是用Hexo吧,推荐一个主题:hexo-theme-huweihuang 真香
+
+# Hexo主题hexo-theme-huweihuang 的效果图
+
+![hexo-theme-huweihuang](/img/huweihuang-theme.png)
+
+# 用别人开源的主题总归要推广一下吧
+https://github.com/huweihuang/hexo-theme-huweihuang
